891a658103eacbcb494d7e0270d3c24c92c209e5
Samuel Cornet <samuel@noukod.com>
2019-02-28 18:31:25 +0000

    Automatically backed up by Learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -3,7 +3,7 @@ class GithubService
   attr_reader :access_token
 
   def initialize(access_hash = nil)
-    @access_token = access_hash if access_hash
+    @access_token = access_hash["access_token"] if access_hash
   end
 
   def authenticate!(client_id, client_secret, code)
@@ -21,7 +21,6 @@ class GithubService
   end
 
   def get_repos
-    # binding.pry
     response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
     repos_array = JSON.parse(response.body)
     repos_array.map{|repo| GithubRepo.new(repo) }
