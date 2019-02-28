bafe6ad79f1552ebce2f5305f081b86d4a9313f9
Samuel Cornet <samuel@noukod.com>
2019-02-28 18:17:17 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,10 +15,10 @@ class SessionsController < ApplicationController
 end
 
 
-  
+
 # class SessionsController < ApplicationController
 #   skip_before_action :authenticate_user, only: :create
-# 
+#
 #   def create
 #     github = GithubService.new
 #     session[:token] = github.authenticate!(ENV["GITHUB_CLIENT"], ENV["GITHUB_SECRET"], params[:code])
